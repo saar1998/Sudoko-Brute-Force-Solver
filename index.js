a600913e0055efb53b985eb68580980ceb09ef16
saar <saar6719@gmail.com>
2019-09-30 16:24:16 +0300

    Add index functionality

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const SolveSudoko = require('./build/Release/SudokoSolver.node');
 
-module.exports = SolveSudoko;
+let solver = {};
+
+solver.solve = (arr) => {
+  const initialObject = internals.convertArrayToObject(arr);
+  const solvedObject = SolveSudoko.solve(initialObject);
+  return internals.convertObjectToArray(solvedObject);
+}
+
+let internals = {};
+
+internals.convertArrayToObject = (arr) => {
+  let obj = {};
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      obj[`${i}${j}`] = arr[i][j];
+    }
+  }
+  return obj;
+};
+
+internals.convertObjectToArray = (obj) => {
+  let arr = [[], [], [], [], [], [], [], [], []];
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      arr[i].push(obj[`${i}${j}`]);
+    }
+  }
+  return arr;
+}
+
+module.exports = solver;
