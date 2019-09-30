76a5cb1871f7ab1b312faea6eb76581072b4bdbb
saar <saar6719@gmail.com>
2019-09-30 23:03:25 +0300

    Add tests for board validation

--- test/mainTests.js
+++ test/mainTests.js
@@ -13,4 +13,16 @@ describe('main tests', () => {
   it('should throw an error when provided with unsolveable board', () => {
     Expect(Solve.bind(this, data.unsolveableBoard)).to.throw('Impossible Board');
   });
+
+  it('should throw an error when provided with board with invalid rows number ', () => {
+    Expect(Solve.bind(this, data.boardWithInvalidRowsNumber)).to.throw('Provided array is of no proper length');
+  });
+
+  it('should throw an error when provided with board with invalid columns number ', () => {
+    Expect(Solve.bind(this, data.boardWithInvalidColumnsNumber)).to.throw('Provided array is of no proper length');
+  });
+
+  it('should throw an error when provided with board with invalid number ', () => {
+    Expect(Solve.bind(this, data.boardWithInvalidNumber)).to.throw('One of the provided numbers is out of range');
+  });
 });
